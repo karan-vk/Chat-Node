d4bc70f201236a8eea99c52215e3374b668d9c23
Karan V <karanvknarayanan@gmail.com>
2020-10-03 11:04:13 +0530

    messges implemnted

--- Public/js/main.js
+++ Public/js/main.js
@@ -1,4 +1,25 @@
 const socket = io();
+const chatMessages = document.querySelector(".chat-messages");
+const chatForm = document.getElementById("chat-form");
 socket.on("message", (message) => {
+  outputMessage(message);
   console.log(message);
+  chatMessages.scrollTop = chatMessages.scrollHeight;
 });
+
+chatForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const msg = e.target.elements.msg.value;
+  socket.emit("chatMessage", msg);
+  e.target.elements.msg.value = "";
+  e.target.elements.msg.focus();
+});
+const outputMessage = (message) => {
+  const div = document.createElement("div");
+  div.classList.add("message");
+  div.innerHTML = `<p class="meta">Mary <span>9:15pm</span></p>
+  <p class="text">
+      ${message}
+  </p>`;
+  document.querySelector(".chat-messages").appendChild(div);
+};
--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 const express = require("express");
 const app = express();
 const http = require("http");
+const { disconnect } = require("process");
 const server = http.createServer(app);
 const socketio = require("socket.io");
 const io = socketio(server);
 app.use(express.static("Public"));
 
 io.on("connection", (socket) => {
-  console.log("new Web server connection");
+  //   console.log("new Web server connection");
   socket.emit("message", "Welcome to chatTY");
+
+  socket.broadcast.emit("message", "A user has joined the chat");
+  socket.on("disconnect", () => {
+    io.emit("message", "A user has left the chat");
+  });
+
+  socket.on("chatMessage", (msg) => {
+    io.emit("message", msg);
+  });
 });
 
 server.listen(process.env.PORT || 3000, function() {
